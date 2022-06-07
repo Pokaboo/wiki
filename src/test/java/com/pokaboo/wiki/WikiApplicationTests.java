c5ef08e9f4ccbc132dc6e71bbb5a70b4d2f5ea9b
Pokaboo <1152401192@qq.com>
2022-06-07 21:38:17 +0800

    【add】项目初始化

---
+++ src/test/java/com/pokaboo/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokaboo.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
